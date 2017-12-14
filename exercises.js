f6eb35db27843b3235c4caecbc57a405404f520d
Nick <nicnhines3@gmail.com>
2017-12-13 18:18:15 -1000

    finsihed

--- exercises.js
+++ exercises.js
@@ -575,7 +575,13 @@ var frequencyMap=function(str){
  * @param {String}
  * @return {Bool}
  */
-var tupleConvertToObject;
+var tupleConvertToObject=function(arr){
+  var obj= {};
+  for(var i=0;i<arr.length;i++){
+    obj[arr[i][0]] = arr[i][1];
+  }
+  return obj;
+};
 
 
 module.exports = {
